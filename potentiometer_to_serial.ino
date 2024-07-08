b0ead20a84b5ff746e229804fe5b88f594d5c21f
CeeBeeL <c.blervacques@free.fr>
2024-07-09 01:49:29 +0200

    init

--- potentiometer_to_serial.ino
+++ potentiometer_to_serial.ino
@@ -1,37 +1,38 @@
-const int numSamples = 32;
-const int numPots = 5;
-
-int values[numPots] = {0};
-int last_values[numPots] = {0};
-
-// GPIO pins: {36, 39, 32, 33, 34, 35, 4, 0, 2, 15, 13, 12, 14, 27, 25};
-const int potPins[numPots] = {36, 39, 32, 33, 34};
+const int NUM_SAMPLES = 32;
+const int NUM_POTS = 5;
+int values[NUM_POTS] = {0};
+int last_values[NUM_POTS] = {0};
+const int pot_pins[NUM_POTS] = {36, 39, 32, 33, 34};
 
 void setup()
 {
     Serial.begin(115200);
 }
 
+void send_midi_cc(byte channel, byte cc, byte value)
+{
+    Serial.write(0xB0 | (channel & 0x0F)); // 0xB0 to 0xBF for CC on channels 1-16
+    Serial.write(cc & 0x7F);               // CC number (0-127)
+    Serial.write(value & 0x7F);            // Value (0-127)
+}
+
 void loop()
 {
-    for (int pot = 0; pot < numPots; pot++)
+    for (int pot = 0; pot < NUM_POTS; pot++)
     {
         long sum = 0;
-        for (int i = 0; i < numSamples; i++)
+        for (int i = 0; i < NUM_SAMPLES; i++)
         {
-            sum += analogRead(potPins[pot]);
-            // delayMicroseconds(100);
+            sum += analogRead(pot_pins[pot]);
         }
-        values[pot] = sum / numSamples;
-        values[pot] = map(values[pot], 0, 4095, 0, 255);
+        values[pot] = sum / NUM_SAMPLES;
+        values[pot] = map(values[pot], 0, 4095, 0, 127); // Map to MIDI range 0-127
 
         if (values[pot] != last_values[pot])
         {
             last_values[pot] = values[pot];
-            Serial.print("0:0,255:255,Pot"); // Plotter format
-            Serial.print(pot + 1);           // Plotter format
-            Serial.print(":");               // Plotter format
-            Serial.println(values[pot]);
+            // Send MIDI CC message
+            send_midi_cc(0, pot, values[pot]); // MIDI channel 1 (0), CC0 to CC4, value 0-127
         }
     }
 }
